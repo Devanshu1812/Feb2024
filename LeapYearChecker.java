27c076b524dbd4e56c0c7e809e8eddc42ce70e7d
devanshu <devanshugupta768@gmail.com>
2024-02-05 10:01:15 +0530

    added excep

--- LeapYearChecker.java
+++ LeapYearChecker.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
 
-public class LeapYearChecker {
+ class LeapYearChecker throws IOException
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException
+     {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a year: ");
