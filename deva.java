26e4b2bab7d7619653d532c1c9c69777b9560085
devanshu <devanshugupta768@gmail.com>
2024-02-06 23:37:55 +0530

    product of 3 numbers program

---
+++ deva.java
@@ -0,0 +1,14 @@
+public class deva {
+    public static void main(String[] args) {
+        // Define the numbers
+        int num1 = 56;
+        int num2 = 67;
+        int num3 = 87;
+
+        // Multiply the numbers
+        int result = num1 * num2 * num3;
+
+        // Print the result
+        System.out.println("The result of multiplying " + num1 + ", " + num2 + ", and " + num3 + " is: " + result);
+    }
+}
